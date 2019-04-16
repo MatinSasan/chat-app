15f90955f1a1db71b335c12c1dedb348aa06a220
Matin Sasanpour <matin.sasan@gmail.com>
2019-04-16 08:26:14 +0430

    Updates

---
+++ precache-manifest.53dd6138fe3b7668001e87eadc5f86ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "925bf40d901ee22fb9b1",
+    "url": "/chat-app/static/js/runtime~main.925bf40d.js"
+  },
+  {
+    "revision": "e5a0205e79da4103ed64",
+    "url": "/chat-app/static/js/main.e5a0205e.chunk.js"
+  },
+  {
+    "revision": "1c38409e3cff9b0e642e",
+    "url": "/chat-app/static/js/2.1c38409e.chunk.js"
+  },
+  {
+    "revision": "e5a0205e79da4103ed64",
+    "url": "/chat-app/static/css/main.1c231d95.chunk.css"
+  },
+  {
+    "revision": "d0a6bd358e61f49ea6f6099d0c9f860c",
+    "url": "/chat-app/index.html"
+  }
+];
